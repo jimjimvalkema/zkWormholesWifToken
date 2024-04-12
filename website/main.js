f3c833b23e8b4c624869f3de263827f7da494fc2
jjvalkema <jjvalkema@protonmail.com>
2024-04-12 14:25:03 +0200

    followed web app tutorial for noir js

--- website/main.js
+++ website/main.js
@@ -1,24 +1,43 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import circuit from "../circuit/target/circuit.json"
+console.log(circuit)
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import  {BarretenbergBackend} from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
 
-setupCounter(document.querySelector('#counter'))
+const setup = async () => {
+  await Promise.all([
+    import("@noir-lang/noirc_abi").then(module => 
+      module.default(new URL("@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm", import.meta.url).toString())
+    ),
+    import("@noir-lang/acvm_js").then(module => 
+      module.default(new URL("@noir-lang/acvm_js/web/acvm_js_bg.wasm", import.meta.url).toString())
+    )
+  ]);
+}
+
+function display(container, msg) {
+  const c = document.getElementById(container);
+  const p = document.createElement('p');
+  p.textContent = msg;
+  c.appendChild(p);
+}
+
+document.getElementById('submitGuess').addEventListener('click', async () => {
+  try {
+    // here's where love happens
+    const backend = new BarretenbergBackend(circuit);
+    const noir = new Noir(circuit, backend);
+    const x = parseInt(document.getElementById('guessInput').value);
+
+    const input = { x, y: 2 };
+    await setup(); // let's squeeze our wasm inits here
+
+    display('logs', 'Generating proof... ⌛');
+    const proof = await noir.generateProof(input); //generateProof in docs
+    display('logs', 'Generating proof... ✅');
+    display('results', proof.proof);
+  } catch(err) {
+    console.log(err)
+    display("logs", "Oh 💔 Wrong guess")
+  }
+});
